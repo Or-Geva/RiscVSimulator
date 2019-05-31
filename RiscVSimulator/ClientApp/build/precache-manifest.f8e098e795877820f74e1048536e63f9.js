4f7f0310cb1d0aff014b5e5ffe2c2eada45e849b
Posty <ortil1@gmail.com>
2019-05-31 09:48:48 +0300

    frontend

---
+++ RiscVSimulator/ClientApp/build/precache-manifest.f8e098e795877820f74e1048536e63f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "43fe397e4e95b8582789aaf066fff104",
+    "url": "/static/media/RISC-V-logo.43fe397e.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "986a0651ed0383f315f6",
+    "url": "/static/js/main.986a0651.chunk.js"
+  },
+  {
+    "revision": "11c31b81833a3ee2f886",
+    "url": "/static/js/2.11c31b81.chunk.js"
+  },
+  {
+    "revision": "986a0651ed0383f315f6",
+    "url": "/static/css/main.7602d01c.chunk.css"
+  },
+  {
+    "revision": "11c31b81833a3ee2f886",
+    "url": "/static/css/2.f78fb584.chunk.css"
+  },
+  {
+    "revision": "a430dee75c2c96d7e16f0479082c88e8",
+    "url": "/index.html"
+  }
+];
